a0324bbb9e20c55e1d7c824e81d9da0da2046c35
DESKTOP-LIBN1TN\rouge <se7ne_886@163.com>
2019-11-01 09:55:08 +0800

    假如readme 和指引;

--- src/api/apiconfig.js
+++ src/api/apiconfig.js
@@ -4,9 +4,9 @@ const HEWEATHER_API = 'https://free-api.heweather.com/s6/weather'
 
 module.exports = {
   // 和风天气个人开发key 对外提供;
-  weatherKey: '043d829ed8e744cb846e0c7dd47e93a8',
+  weatherKey: '',
   // 腾讯地图开发key
-  qqMapKey: 'E3HBZ-D2NHU-JSMVX-4EN2N-YKT7F-65FOI',
+  qqMapKey: '',
   // 实时天气接口地址
   nowWeatherUrl: `${HEWEATHER_API}/now`,
   // 逐日天气接口地址
